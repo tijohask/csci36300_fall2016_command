7bf77c008339e775a32a4ac98974821e5d041b02
Timothy Haskins <tijohask@iupui.edu>
2016-10-28 21:29:04 -0400

    Final expected commit of the driver class. Some minor bug fixes, lots of explanatory comments added. Have a pleasant weekend.

--- driver.cpp
+++ driver.cpp
@@ -46,45 +46,52 @@ int main()
 void run_code()
 {
 	std::string input;
-	Queue<std::string> queue;
-//	Queue<Command*> commands;
-	Stack<std::string> stack;
 	bool flag = true;
 	while(true)
 	{	
 		Queue<Command*> commands;
-		flag = true;
+		std::istringstream infix ("");
+		flag = false;
 		printf("Please input an equation:\n");
 		getline(std::cin, input);
-
 		if( input.compare("QUIT") == 0 )
 		{
 			break;
 		}
-		std::istringstream infix (input);
-		flag = check_equation( infix );		
-		infix.clear();
-		if( flag )
-		{
-			infix.clear();
-			infix.str(input);
-			infix_to_command( infix, commands );
+		
+		if( input.compare("") != 0 )
+		{// make sure the user doesn't just input nothing.
+			flag = true;
 		}
-		 
+		
 		if( flag )
-		{
+		{//check the equation to make sure it's valid
+			infix.clear();
+			infix.str( input );
+			flag = check_equation( infix );		
+		}
+		
+		if( flag )
+		{//run the evaluation on the equation
+			infix.clear();
+			infix.str( input );
+			infix_to_command( infix, commands );
 			evaluate( commands );
 		}
-
 	}
 	printf("Goodbye.\n");
 }
 
+/*
+ * Checks the input equation to make sure it can be accepted by the other
+ * methods as a valid equation.
+ */
+
 bool check_equation( std::istringstream & check )
 {
 	std::string token;
-	bool num_next = true;
-	int parens = 0;
+	bool num_next = true; //used to make sure the equation is in proper order
+	int parens = 0;//used to make sure there is an equivalent number of parens
 	while( !check.eof() )
 	{
 		check >> token;
@@ -92,11 +99,11 @@ bool check_equation( std::istringstream & check )
 		if( is_integer(token) )
 		{
 			if ( num_next )
-			{
+			{//if a number is supposed to be next, we're good
 				num_next = false;
 			}
 			else
-			{
+			{//a problem has emerged
 				return false;
 			}
 		}
@@ -107,11 +114,11 @@ bool check_equation( std::istringstream & check )
 		token.compare("%") == 0 )
 		{
 			if ( !num_next )
-			{
+			{//if an operator is supposed to be next, we're good
 				num_next = true;
 			}
 			else
-			{
+			{//a problem has emerged
 				printf( "Error: \"%s\" Operator without numbers.\n", 
 					token.c_str() );
 				return false;
@@ -121,11 +128,11 @@ bool check_equation( std::istringstream & check )
 		{
 			parens += 1;
 			if ( num_next ) 
-			{
+			{//if a number is supposed to be next, a "(" will work fine
 				num_next = true;
 			}
 			else
-			{
+			{//a problem has emerged
 				printf( "Error: Expected Operator.\n" );
 				return false;
 			}
@@ -134,21 +141,22 @@ bool check_equation( std::istringstream & check )
 		{
 			parens -= 1;
 			if ( !num_next )
-			{
+			{//if an operator is supposed to be next, a ")" will work fine.
 				if ( parens < 0 )
-				{
+				{//if more closed parens than open parens have been encountered
+				//at any time, something has gone wrong.
 					printf( "Error: \")\" without \"(\"\n" );
 					return false;
 				}
 			}
 			else
-			{
+			{//a problem has emerged
 				printf( "Error: Invalid equation inside parenthesis.\n" );
 				return false;
 			}
 		}
 		else
-		{
+		{//unrecognized token will not be accepted
 			printf( "Error: %s not recognized.\n", token.c_str() );
 			return false;
 		}
@@ -162,6 +170,8 @@ bool check_equation( std::istringstream & check )
 
 // Ryan: Need to ensure that you provide the correct output and 
 // handle invalid input.
+// FIXED: Tested
+
 
 /*
  * Takes in a stringstream object and updates the queue 
@@ -171,13 +181,17 @@ bool check_equation( std::istringstream & check )
 bool infix_to_command(std::istringstream & infix, Queue<Command*> & queue)
 {
 	std::string token;
-	Stack<Command*> stack;
-	Command *cmd = NULL;
-	Stack_Factory factory;
+	Stack<Command*> stack;//fresh stack for every recursion of this method
+	Command *cmd = NULL;//command pointer for adding to queue or stack
+	Stack_Factory factory;//factory to create commands.
 	while(!infix.eof())
 	{
 		infix >> token;
 		
+		// For any operator,
+		// create command based on token
+		// clear stack based on token
+		// push command onto stack 
 		if( token.compare("+") == 0 )
 		{
 			cmd = factory.create_add_command();
@@ -209,11 +223,11 @@ bool infix_to_command(std::istringstream & infix, Queue<Command*> & queue)
 			stack.push( cmd );
 		}
 		else if( token.compare("(") == 0 )
-		{
+		{//recurse onto itself if "(" is found
 			infix_to_command( infix, queue );
 		}
 		else if( token.compare(")") == 0 )
-		{
+		{//end recurse when ")" is found
 			break;
 		}
 		else
@@ -222,18 +236,16 @@ bool infix_to_command(std::istringstream & infix, Queue<Command*> & queue)
 			queue.enqueue(cmd);
 		}
 	}
-	while( !stack.is_empty() )
-	{
-		queue.enqueue( stack.pop() );
-	}
+	//clear the stack, either at the end of recursion or just the end of the
+	//function
+	clear_stack( 5, stack, queue );
 	return true;
 }
 
 /*
  * Clear the stack, with rules based on the string passed in. If the string
  * is a + or - sign, pop until you see a plus or a minus sign. If the string
- * is a closed parenthesis, pop until you see an open parenthesis. Else, pop 
- * until the stack is empty.
+ * is a *, / or % sign, pop until an equivalent is found. 
  */
 void clear_stack(int prec, Stack<Command*> & stack, Queue<Command*> & take)
 {
@@ -264,16 +276,19 @@ int evaluate(Queue<Command*> queue)
 	Command * cmd;
 	Stack<int> stack;
 	bool flag = true;
+	// Iterate until the queue is empty or a problem emerges
 	while( !queue.is_empty() && flag )
 	{
 		cmd = queue.dequeue();
 		flag = cmd->execute(stack);
 		delete cmd;
 	}
+	// If everything went fine and there's an element on the stack, print it.
 	if( !stack.is_empty() && flag )
 	{
 		printf( "%d\n", stack.pop() );
 	}
+	// If something went wrong, empty the queue and print the error.
 	if( !flag )
 	{
 		while( !queue.is_empty() )
